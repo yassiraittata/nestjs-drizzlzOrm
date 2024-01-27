048e5893e55553b332c9fdb0bcf11809ef2f8d32
Yassir aittata <yassir.aittata@gmail.com>
2024-01-27 18:20:56 +0000

    link the db to the app

--- src/drizzle/drizzle.provider.ts
+++ src/drizzle/drizzle.provider.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 
 import * as schema from './schema';
 
-export const DrizzleAsyncProvider = '';
+export const DrizzleAsyncProvider = 'drizzleProvider';
 
 export const drizzleProvider = [
   {
---
+++ src/users/users.service.ts
@@ -0,0 +1,11 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+
+import { DrizzleAsyncProvider } from '../drizzle/drizzle.provider';
+import * as schema from '../drizzle/schema';
+
+@Injectable()
+export class UsersService {
+    constructor(@Inject(DrizzleAsyncProvider) private db: BetterSQLite3Database<typeof schema>) {}
+
+}
