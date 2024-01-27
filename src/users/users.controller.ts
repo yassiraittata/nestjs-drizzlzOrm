9e5ea6cce77ad70cfd45cee000d02b9a4448e908
Yassir aittata <yassir.aittata@gmail.com>
2024-01-27 19:10:24 +0000

    hash the password

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Post()
-  createUser(@Body() Body: any) {
-    return this.userService.createUser();
+  createUser(@Body() body: CreateUserDto) {
+    return this.userService.createUser(body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3';
+import * as argon from 'argon2';
 
 import { DrizzleAsyncProvider } from '../drizzle/drizzle.provider';
 import * as schema from '../drizzle/schema';
+import { CreateUserDto } from './dto';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
 export class UsersService {
@@ -11,5 +14,19 @@ export class UsersService {
     private db: BetterSQLite3Database<typeof schema>,
   ) {}
 
-  createUser() {}
+  async createUser(dto: CreateUserDto) {
+    const user = await this.db.query.users.findFirst({
+      where: eq(schema.users.email, dto.email),
+    });
+
+    if (user) throw new BadRequestException('User already exists!');
+
+    const hash = await argon.hash(dto.password);
+
+    const newUser = this.db
+      .insert(schema.users)
+      .values({ name: dto.name, email: dto.email, password: hash });
+
+    return newUser;
+  }
 }
